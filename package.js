00582c1a38326dc5d65ab58547b373bf13760be1
jankapunkt <jkuester@uni-bremen.de>
2017-09-13 15:17:02 +0200

    package update with tests

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'danimal:userpresence',
-  version: '1.0.1',
+  version: '1.0.2',
   summary: 'Track user online status across multiple servers.',
   git: 'https://github.com/dan335/meteor-user-presence',
   documentation: 'README.md'
@@ -16,6 +16,24 @@ Package.onUse(function(api) {
   api.addFiles('userPresence.js', 'server');
   api.export([
     'UserPresenceSessions',
-    'UserPresenceServers'
+    'UserPresenceServers',
+    'UserPresenceHelpers',
   ], 'server');
+});
+
+Package.onTest(function (api) {
+
+  api.use([
+    'meteor',
+    'ecmascript',
+    'mongo',
+    'random',
+	'accounts-base',
+	'accounts-password',
+    'johanbrook:publication-collector',
+    'dburles:mongo-collection-instances',
+    'practicalmeteor:chai',
+    'danimal:userpresence',
+  ]);
+  api.mainModule('userPresence.tests.js', 'server');
 });
